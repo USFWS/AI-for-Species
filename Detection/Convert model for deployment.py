b63877fd42a16c2a5e76fac15b74549ee7668453
Brad Pickens <bradley_pickens@fws.gov>
2026-06-25 14:05:18 -0400

    update with config usage

--- Detection/Convert model for deployment.py
+++ Detection/Convert model for deployment.py
@@ -4,21 +4,14 @@ import openvino
 import onnxruntime as ort
 import tensorrt
 
-#class UltralyticsDetectionModel(DetectionModel):
-  #  """Detection model for Ultralytics YOLO models.
-   # Supports PyTorch (.pt), ONNX (.onnx), OpenVINO (.xml or _openvino_model/),
-    #NCNN (.param or _ncnn_model/), and TorchScript (.torchscript) models.
-    #"""
+import config
 
-model_path = "E:/WHCR_2025/whcr_detector_yolo11s_Aug3.pt"
-# Load the latest YOLO26 model
+model_path = config.MODEL_PATH
 model = YOLO(model_path)
 
 # Export the model to ## format with half-precision enabled
 # The 'half=True' argument converts weights to FP16
 model.export(format= "onnx", half=True)
-model.export(format= "engine", half=True)
+model.export(format= "engine", half=True, device= "cuda:0", imgsz=1024, workspace=6,
+simplify=True, batch=1)
 model.export(format= "openvino", half=True)
-
-
-
