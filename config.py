d58f51f5086db8123f766d947f3fcab9e27d6565
bpickens99 <bradley_pickens@fws.gov>
2026-09-07 10:54:18 -0400

    minor reorg

---
+++ config.py
@@ -0,0 +1,34 @@
+
+# !! Add this file to .gitignore so paths are never pushed to GitHub !!
+CSV_DATA = "H:/WHCR_2025/3_survey_infer/next_up.csv"
+
+CSV_GROUND_TRUTH = "H:/WHCR_2025/2_detection/results_Aug20/val_ground_truth.csv"
+CSV_PREDICTIONS = "H:/WHCR_2025/2_detection/Aug2026/results_Aug20/yolo11_s_conf0.15_overlap_0.05.csv"
+
+crops_w_context = "H:/WHCR_2025/1_classify/DATASET_classify_v6/classify_test_v6.csv"
+EXPORT_DIR = "H:/WHCR_2025/3_survey_infer/next_up/"
+
+EXPORT_CONTEXT_BIRD = "H:/WHCR_2025/3_survey_infer/detects_w_context/"
+EXPORT_INFER_BIRD = "H:/WHCR_2025/3_survey_infer/detects_for_infer/"
+
+JSON_INPUT =  "D:/whcr_parents/JPG_20250122.json"
+JSON_OUTPUT =  "D:/whcr_parents/JPG_20250122.json"
+
+MODEL_PATH_DETECT = "H:/WHCR_2025/2_detection/yolo11s_aug19_final_model/weights/best.fp16.onnx"
+
+MODEL_PATH_CLASSIFY = 'H:/WHCR_2025/1_classify/whcr_classify_swin_s_v6_2026.pt'
+
+NEW_CSV = "H:/WHCR_2025/infer_demo/"
+
+NEW_CSV_DIR = "H:/WHCR_2026/3_survey_infer/csv_results_Aug2026/"
+
+PICKLE_DIR = "D:/whcr_parents/JPG_20250122_131500_results_reverse/exp/pickles/"
+
+PROJECT_NAME = "H:/WHCR_2026/survey_detections/"
+
+SOURCE_IMG = "E:/WHCR_2026/next_up1/"
+
+VISUAL_PATH = "D:/whcr_parents/JPG_20250122/"
+YOLO_OUTPUT_DIR = "D:/whcr_parents/JPG_20250122/"
+
+WORKING_DIR = "D:/whcr_parents/JPG_20250122/"
